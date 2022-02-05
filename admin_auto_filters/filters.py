1619fc0283b00167ff1252b0a4c47a57de7a2e37
sankar datta <sankardatt@gmail.com>
2022-02-05 14:32:05 +0100

    Bugfix: Fix AttributeError: 'NoneType' object has no attribute 'model' on django admin page load <django version 3.2 and python 3.10>

--- admin_auto_filters/filters.py
+++ admin_auto_filters/filters.py
@@ -16,6 +16,41 @@ class AutocompleteSelect(Base):
     def get_url(self):
         return self.custom_url if self.custom_url else super().get_url()
 
+    def optgroups(self, name, value, attr=None):
+        """Return selected options based on the ModelChoiceIterator."""
+        default = (None, [], 0)
+        groups = [default]
+        has_selected = False
+        selected_choices = {
+            str(v) for v in value
+            if str(v) not in self.choices.field.empty_values
+        }
+        if not self.is_required and not self.allow_multiple_selected:
+            default[1].append(self.create_option(name, '', '', False, 0))
+
+        if self.field.remote_field:
+            remote_model_opts = self.field.remote_field.model._meta
+            to_field_name = getattr(self.field.remote_field, 'field_name',
+                                    remote_model_opts.pk.attname)
+        else:
+            remote_model_opts = self.field.model._meta
+            to_field_name = getattr(self.field, 'field_name', remote_model_opts.pk.attname)
+        to_field_name = remote_model_opts.get_field(to_field_name).attname
+        choices = (
+            (getattr(obj, to_field_name), self.choices.field.label_from_instance(obj))
+            for obj in self.choices.queryset.using(self.db).filter(**{'%s__in' % to_field_name: selected_choices})
+        )
+        for option_value, option_label in choices:
+            selected = (
+                str(option_value) in value and
+                (has_selected is False or self.allow_multiple_selected)
+            )
+            has_selected |= selected
+            index = len(default[1])
+            subgroup = default[1]
+            subgroup.append(self.create_option(name, option_value, option_label, selected_choices, index))
+        return group
+
 
 class AutocompleteFilter(admin.SimpleListFilter):
     template = 'django-admin-autocomplete-filter/autocomplete-filter.html'
